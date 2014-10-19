e7d360c86de8f45a9e62e9ca81281031dfcd57a1
Nishant <nishantpathak.cse@gmail.com>
2014-10-19 14:09:41 +0530

    init commit

---
+++ includes/footer.php
@@ -0,0 +1,18 @@
+<?php
+?>
+    </div>
+
+    <div id="footer">
+      <div class="container">
+        <p class="text-muted credit"> KitneGandeHainHum © 2014<a href="/">.</p>
+      </div>
+    </div>
+
+  <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
+  <script type="text/javascript" src="../js/angular.min.js"></script>
+  <script type="text/javascript" src="../bootstrap/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="../js/ui-bootstrap-tpls-0.5.0.min.js"></script>
+  <script type="text/javascript" src="../js/kitnegandehainham.js"></script>
+</body>
+</html>
